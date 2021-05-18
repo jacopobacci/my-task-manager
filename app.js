e6751d474bdb3aa43893c55fd18804d9c8457166
jacopobacci <bacci.jaco@gmail.com>
2021-05-18 15:33:16 +0200

    fix issues

--- app.js
+++ app.js
@@ -1,88 +1,92 @@
-// Task: create a todo app
-
-// 1. Display the message with the questions V
-// 2. Give the possibility to enter a choice with a number input between 1 and 5
-// 3. Develop the logic of the single options
-// 3.1 Show all the tasks
-// 3.2 Add a task
-// 3.3 Delete a task
-// 3.4 Mark the task as done
-// 3.5 Exit the task manager
-
 let allTasks = [];
-let selectP = document.querySelector('.add-task-before');
 
 const taskManager = () => {
   let action = document.querySelector('#action').value;
-  let allTasks = [];
+  let alert = document.querySelector('.alert');
+  let alertDeleteEl = document.querySelector('.alert-delete');
+
   if (action == 1 && allTasks.length === 0) {
-    addTaskBefore();
+    createAlert();
   } else if (action == 1 && allTasks.length !== 0) {
-    if (selectP) {
-      selectP.remove();
-    }
+    removeEl(alert);
+    removeEl(alertDeleteEl);
     const list = document.querySelector('.task-list');
     list.style.setProperty('display', 'block');
   } else if (action == 2) {
-    if (selectP) {
-      selectP.remove();
-    }
-    document.querySelector('.add-task').style.setProperty('display', 'block');
+    removeEl(alert);
+    removeEl(alertDeleteEl);
+    displayEl('.add-task');
   } else if (action == 3) {
-    if (selectP) {
-      selectP.remove();
+    removeEl(alert);
+    if (allTasks.length !== 0) {
+      displayEl('.delete-task');
+    } else {
+      alertDelete();
     }
-    document.querySelector('.delete-task').style.setProperty('display', 'block');
   } else if (action == 4) {
-    if (selectP) {
-      selectP.remove();
-    }
-    document.querySelector('.task-done').style.setProperty('display', 'block');
+    removeEl(alert);
+    removeEl(alertDeleteEl);
+    displayEl('.task-done');
   } else {
-    if (selectP) {
-      selectP.remove();
-    }
+    removeEl(alert);
+    removeEl(alertDeleteEl);
     exitProgram();
   }
+  reset('#action');
 };
 
-const addTaskBefore = () => {
+const reset = (el) => {
+  document.querySelector(el).value = '';
+};
+
+const displayEl = (el) => {
+  document.querySelector(el).style.setProperty('display', 'block');
+};
+
+const removeEl = (el) => {
+  el && el.remove();
+};
+
+const createAlert = () => {
   const p = document.createElement('p');
-  p.className = 'add-task-before';
+  p.className = 'alert';
   p.innerText = 'Add one or more task to display the task list...';
+  p.style.setProperty('color', 'red');
+  const enterNumberForm = document.querySelector('.enter-number');
+  enterNumberForm.append(p);
+};
+const alertDelete = () => {
+  const p = document.createElement('p');
+  p.className = 'alert-delete';
+  p.innerText = 'You must have created tasks to delete one of them';
+  p.style.setProperty('color', 'red');
   const enterNumberForm = document.querySelector('.enter-number');
   enterNumberForm.append(p);
 };
 
 const addTask = () => {
-  // extract the value of text
   let singleTask = document.getElementById('task').value;
-  // push into tasks array
   allTasks.push(singleTask);
-  // select empty list html
   const list = document.querySelector('.task-list');
   list.style.setProperty('display', 'block');
-  // create list item
   const item = document.createElement('li');
-  // insert the text into it
-  item.innerText = `${singleTask}`;
-  // render into the html
+  localStorage.setItem('task', `${singleTask}`);
+  item.innerText = `${localStorage.getItem('task')}`;
   list.append(item);
+  reset('#task');
 };
 
 const deleteTask = () => {
   const list = document.querySelector('.task-list');
   list.style.setProperty('display', 'block');
-  // extract which task you want to delete and find the index
   let taskToDelete = document.getElementById('delete').value;
-  // delete the task
   if (taskToDelete - 1 > -1) {
     allTasks.splice(taskToDelete, 1);
   }
-  // display the tasks
   console.log(allTasks);
   const item = document.querySelector(`.task-list li:nth-of-type(${taskToDelete})`);
   item.remove();
+  reset('#delete');
 };
 
 const taskDone = () => {
@@ -90,11 +94,11 @@ const taskDone = () => {
   list.style.setProperty('display', 'block');
   let completedTask = document.getElementById('done').value;
   const item = document.querySelector(`.task-list li:nth-of-type(${completedTask})`);
-  // apply line through to the element
   item.style.setProperty('text-decoration', 'line-through');
+  reset('#done');
 };
 
 const exitProgram = () => {
-  document.querySelector('.exit-message').style.setProperty('display', 'block');
+  document.querySelector('.exit-message').style.cssText = 'display: block;color:red';
   document.querySelector('.task-manager').style.setProperty('display', 'none');
 };
